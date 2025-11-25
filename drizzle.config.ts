fd3cef418a3392e108e45bcc9081e9749811f173
Wagih-13 <wagiha498@gmail.com>
2025-11-25 03:05:52 +0200

    production 2

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -11,9 +11,9 @@ export default {
   dbCredentials: {
     host: process.env.DB_HOST || "localhost",
     port: Number(process.env.DB_PORT) || 5432,
-    user: process.env.DB_USER || "postgres",
-    password: process.env.DB_PASSWORD || "Yarasara+ahmed",
-    database: process.env.DB_NAME || "nextecom_db",
+    user: process.env.DB_USER || "wagih",
+    password: process.env.DB_PASSWORD || "AlAl@!@!12!@",
+    database: process.env.DB_NAME || "modestwear_db",
     ssl: false,
   },
   verbose: true,
